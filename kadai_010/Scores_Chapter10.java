8771a11ca2180d45b8a673008c9a0a543793b474
ogasoma <154291837+ogasoma@users.noreply.github.com>
2024-03-07 17:42:11 +0900

    Add files via upload

--- kadai_010/Scores_Chapter10.java
+++ kadai_010/Scores_Chapter10.java
@@ -2,6 +2,7 @@ package kadai_010;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -17,12 +18,12 @@ public class Scores_Chapter10 {
         try {
             // データベースに接続
             con = DriverManager.getConnection(
-                "jdbc:mysql://localhost/challenge_java/scores",
+                "jdbc:mysql://localhost/challenge_java",
                 "root",
                 "Pass12345"
             );
 
-            System.out.println("データベース接続成功" + con);
+            System.out.println("データベース接続成功:" + con);
 
             // SQLクエリを準備
             statement = con.createStatement();
@@ -30,13 +31,23 @@ public class Scores_Chapter10 {
  
 
             // SQLクエリを実行（DBMSに送信）
-            System.out.println("レコード更新を実行します" + statement.toString() );
+            System.out.println("レコード更新を実行します" );
             int rowCnt = statement.executeUpdate(sql);
             System.out.println( rowCnt + "件のレコードが更新されました");
             
             System.out.println("数学・英語の点数が高い順に並べ替えました");
-
-            SELECT SELECT * age FROM scores ORDER BY score_math ASC;
+            String sql2 = "SELECT * FROM scores ORDER BY score_math DESC , score_english DESC" ;
+            ResultSet result = statement.executeQuery(sql2);
+            
+            // SQLクエリの実行結果を抽出
+            while(result.next()) {
+                int id = result.getInt("id");
+                String name = result.getString("name");
+                int score_math = result.getInt("score_math");
+                int score_english = result.getInt("score_english");
+                System.out.println(result.getRow() + "件目:生徒ID=" + id
+                                   + "／氏名=" + name + "／数学=" + score_math + "／英語=" + score_english  );
+            }
 
         } catch(SQLException e) {
             System.out.println("エラー発生：" + e.getMessage());
